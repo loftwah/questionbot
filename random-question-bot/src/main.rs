27413d74aae532d1b76e8168f3a2ec240eb779e8
Dean Lofts <dean@deanlofts.xyz>
2023-02-15 17:15:45 +1100

    add discord question bot

---
+++ random-question-bot/src/main.rs
@@ -0,0 +1,36 @@
+use rand::seq::SliceRandom;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use std::fs;
+use std::error::Error;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Question {
+    question: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let questions_file = fs::read_to_string("QUESTIONS")?;
+    let questions: Vec<Question> = serde_json::from_str(&questions_file)?;
+
+    let mut rng = rand::thread_rng();
+    let shuffled_questions = questions.choose_multiple(&mut rng, questions.len()).cloned().collect::<Vec<_>>();
+    let question = &shuffled_questions[0];
+
+    let question_text = &question.question;
+
+    let json = json!({
+        "content": question_text,
+        "username": "Random Question Bot"
+    });
+
+    let client = reqwest::Client::new();
+    let res = client.post("WEBHOOK")
+        .json(&json)
+        .send()
+        .await?;
+    println!("{:?}", res);
+
+    Ok(())
+}
